bbc7d0804e804576228a0553a9c14a28713e9aed
ppconde <pepconde.1993@gmail.com>
2016-12-06 11:58:45 +0000

    Implement Image Reader
    Image Reader not fully working yet

--- RSI Project/RSI Project/Form1.cs
+++ RSI Project/RSI Project/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace RSI_Project
 {
@@ -15,6 +16,40 @@ namespace RSI_Project
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "BMP Files|*.bmp";
+            openFileDialog1.Title = "Select a BMP file";
+
+            // Show the Dialog.
+            // If the user clicked OK in the dialog and
+            // a .CUR file was selected, open it.
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //string startupPath = Environment.CurrentDirectory;
+                //string directoryPath = System.IO.Path.GetDirectoryName(startupPath);
+                //int numbFiles = openFileDialog1.FileName.Length;
+                Bitmap[] images;
+                for(int i=1; i<openFileDialog1.FileName.Length; i++)
+                {
+                    images[i] = openFileDialog1.FileNames.Select(fn => new Bitmap(fn)).ToArray();
+                    //Estou com dificuldades em armazenar um array de imagens. Ele tem problemas na classe Bitmap.
+                    //Não sei se devíamos usar o foreach e uma lista com as imagens carregadas invés.
+                }
+            }
+
+        }
+        public static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
         }
     }
 }
